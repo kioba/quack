8a62ccffeef615eb3a220d7e5a20028a8a9b2d54
kioba <charlssom@gmail.com>
2019-05-19 18:23:27 +0100

    ADD: update build.gradle for placeholder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,25 +1,25 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("kotlin-android-extensions")
+  id("com.android.application")
+  id("kotlin-android")
+  id("kotlin-kapt")
+  id("kotlin-android-extensions")
 }
 apply(from = "../buildSrc/android.kts")
 
 android {
-    defaultConfig {
-        applicationId = "io.github.kioba.jsonplaceholder"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+  defaultConfig {
+    applicationId = "io.github.kioba.jsonplaceholder"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+  implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation(project(":feed"))
-    implementation(project(":core"))
+  implementation(project(":feed"))
+  implementation(project(":core"))
 
-    Dependencies.standard.forEach { implementation(it) }
+  Dependencies.standard.forEach { implementation(it) }
   Dependencies.standardKapt.forEach { kapt(it) }
 
   Dependencies.testStandard.forEach { testImplementation(it) }
