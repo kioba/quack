a49a012f89310c64fe6d6d8c7b0db573c8d9b643
kioba <charlssom@gmail.com>
2019-05-19 09:12:31 +0100

    ADD: doc

--- core/src/main/java/io/github/kioba/core/FragmentExtensions.kt
+++ core/src/main/java/io/github/kioba/core/FragmentExtensions.kt
@@ -3,7 +3,9 @@ package io.github.kioba.core
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
-
+/**
+ * wraps the FragmentTransaction begin and commit behind a higher order function
+ */
 inline fun FragmentManager.execute(transaction: FragmentTransaction.() -> Unit) {
   beginTransaction().apply(transaction).commit()
 }
