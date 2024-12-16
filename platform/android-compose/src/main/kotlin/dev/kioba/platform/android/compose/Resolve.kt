05148f2a450a6245b5724a673c2ee24fc4ff1bf8
kioba <charlssom@gmail.com>
2024-12-16 12:54:49 +0100

    :lipstick: [#36] Enable Edge to Edge

--- platform/android-compose/src/main/kotlin/dev/kioba/platform/android/compose/Resolve.kt
+++ platform/android-compose/src/main/kotlin/dev/kioba/platform/android/compose/Resolve.kt
@@ -3,10 +3,8 @@ package dev.kioba.platform.android.compose
 import androidx.annotation.IdRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 
 @ReadOnlyComposable
 @Composable
-public fun @receiver:IdRes Int.resolve(): String =
-  stringResource(this)
+public fun @receiver:IdRes Int.resolve(): String = stringResource(this)
