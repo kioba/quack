da892fd54c4b2fbe3d2af495a291443764764ce1
kioba <charlssom@gmail.com>
2025-02-01 20:45:56 +0000

    :camera_flash: [#49] Introduce Snapshot testing

--- feature/feed/src/screenshotTest/kotlin/dev/kioba/feature/feed/FeedUiScreenshotTest.kt
+++ feature/feed/src/screenshotTest/kotlin/dev/kioba/feature/feed/FeedUiScreenshotTest.kt
@@ -1,50 +1,17 @@
 package dev.kioba.feature.feed
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.PreviewDynamicColors
-import androidx.compose.ui.tooling.preview.PreviewFontScale
-import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import dev.kioba.feature.feed.model.FeedState
 import dev.kioba.feature.feed.ui.FeedPreview
 import dev.kioba.feature.feed.ui.FeedUi
+import dev.kioba.platform.android.compose.SnapshotPreview
 
 @Suppress("unused")
 internal class FeedUiScreenshotTest {
-  //
-//  @Preview(
-//    group = "portrait",
-//    showBackground = true,
-//    device = "spec:width=1080px,height=2424px,cutout=double",
-//    showSystemUi = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
-//  )
-//  @Preview(
-//    group = "portrait",
-//    showBackground = true,
-//    device = "spec:width=1080px,height=2424px,cutout=double",
-//    showSystemUi = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
-//  )
-//  @Preview(
-//    group = "landscape",
-//    showBackground = true,
-//    device = "spec:width=1080px,height=2424px,cutout=double,orientation=landscape",
-//    showSystemUi = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
-//  )
-//  @Preview(
-//    group = "landscape",
-//    showBackground = true,
-//    device = "spec:width=1080px,height=2424px,cutout=double,orientation=landscape",
-//    showSystemUi = true,
-//    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
-//  )
-  @PreviewScreenSizes
-  @PreviewFontScale
-  @PreviewLightDark
-  @PreviewDynamicColors
+
+
+  @SnapshotPreview
   @Composable
   fun FeedScreenshotTest(
     @PreviewParameter(FeedPreview::class) state: FeedState,
