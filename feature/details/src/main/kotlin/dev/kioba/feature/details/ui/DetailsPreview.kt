0cd3ed27d3c03d006bb746a8560306735661a1dc
kioba <charlssom@gmail.com>
2025-04-28 15:42:06 +0100

    :sparkles: Add fake data generation for comments in testing

--- feature/details/src/main/kotlin/dev/kioba/feature/details/ui/DetailsPreview.kt
+++ feature/details/src/main/kotlin/dev/kioba/feature/details/ui/DetailsPreview.kt
@@ -1,10 +1,20 @@
 package dev.kioba.feature.details.ui
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import dev.kioba.domain.post.fakes.givenCommentBody
+import dev.kioba.domain.post.fakes.givenCommentId
+import dev.kioba.domain.post.fakes.givenCommentIdOne
+import dev.kioba.domain.post.fakes.givenCommentIdTwo
+import dev.kioba.domain.post.fakes.givenCommentUserEmail
+import dev.kioba.domain.post.fakes.givenCommentUserEmailOne
+import dev.kioba.domain.post.fakes.givenCommentUserName
+import dev.kioba.domain.post.fakes.givenCommentUserNameOne
 import dev.kioba.domain.user.fakes.givenAvatar
 import dev.kioba.domain.user.fakes.givenName
 import dev.kioba.domain.user.fakes.givenPostBody
 import dev.kioba.domain.user.fakes.givenPostTitle
+import dev.kioba.feature.details.model.CommentUserViewState
+import dev.kioba.feature.details.model.CommentViewState
 import dev.kioba.feature.details.model.DetailsContentViewState
 import dev.kioba.feature.details.model.DetailsViewState
 import dev.kioba.feature.details.model.PostAndUserViewState
@@ -36,7 +46,50 @@ internal class DetailsPreview : PreviewParameterProvider<DetailsViewState> {
             body = givenPostBody(),
           ),
         ),
-        comments = emptyList(),
+        comments = listOf(
+          givenCommentViewState(),
+          givenCommentViewStateOne(),
+          givenCommentViewStateTwo(),
+        ),
       ),
     )
 }
+
+private fun TestDataScope.givenCommentViewState(): CommentViewState =
+  CommentViewState(
+    id = givenCommentId().value,
+    user = givenCommentUserViewState(),
+    body = givenCommentBody(),
+  )
+
+private fun TestDataScope.givenCommentUserViewState(): CommentUserViewState =
+  CommentUserViewState(
+    email = givenCommentUserEmail().value,
+    name = givenCommentUserName().value,
+  )
+
+private fun TestDataScope.givenCommentViewStateOne(): CommentViewState =
+  CommentViewState(
+    id = givenCommentIdOne().value,
+    user = givenCommentUserViewStateOne(),
+    body = givenCommentBody(),
+  )
+
+private fun TestDataScope.givenCommentUserViewStateOne(): CommentUserViewState =
+  CommentUserViewState(
+    email = givenCommentUserEmailOne().value,
+    name = givenCommentUserNameOne().value,
+  )
+
+private fun TestDataScope.givenCommentViewStateTwo(): CommentViewState =
+  CommentViewState(
+    id = givenCommentIdTwo().value,
+    user = givenCommentUserViewStateTwo(),
+    body = givenCommentBody(),
+  )
+
+private fun TestDataScope.givenCommentUserViewStateTwo(): CommentUserViewState =
+  CommentUserViewState(
+    email = givenCommentUserEmailOne().value,
+    name = givenCommentUserNameOne().value,
+  )
